1c69c4d13eac8a5181bd624ff968ae9214b57a10
leolambo <leoyambo@yahoo.com>
2017-04-08 04:16:05 -0400

    gradle files

--- app/src/main/java/com/example/leo/parq/home.java
+++ app/src/main/java/com/example/leo/parq/home.java
@@ -1,8 +1,17 @@
 package com.example.leo.parq;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,18 +19,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class home extends FragmentActivity implements OnMapReadyCallback {
+public class home extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location startLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            Log.d("Api Client Built", "Done");
+        }
     }
 
 
@@ -37,10 +57,66 @@ public class home extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-38.986918, -76.942554);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Log.d("Map Ready", "Waiting for location");
+        if(startLocation != null) {
+            LatLng here = new LatLng(startLocation.getLatitude(), startLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(here).title("You are here"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(here));
+            Log.d("Map Ready", "your location");
+        } else {
+            LatLng umd = new LatLng(38.986918, -76.942554);
+            mMap.addMarker(new MarkerOptions().position(umd).title("Marker in UMD"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(umd));
+            Log.d("Map Ready", "UMD's location");
+        }
+    }
+
+    protected void onStart() {
+        //Connect to Google Api client
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        //Disconnect to Google Api client
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            // tell user to turn on location
+            Log.d("No Location Access", ":(");
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (location != null) {
+            Log.d("Location Set", ":)");
+            startLocation = location;
+        }
+    }
+    //@Override
+    //public void onConnected(@Nullable Bundle bundle) {
+
+    //}
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
